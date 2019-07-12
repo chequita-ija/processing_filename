22525c3b28011d3f5e31ab90d445efcc00a2410f
ija <41140628+chequita-ija@users.noreply.github.com>
2019-07-12 14:09:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os.path
 import datetime
 
 def processing_filename(dir_filename, timestamp):
-    
     # split()でディレクトリのパスとファイル名を分割
     directory, filename = os.path.split(dir_filename)
     # splitext()で文字列と拡張子を分割
@@ -14,11 +13,9 @@ def processing_filename(dir_filename, timestamp):
     print(name+'_'+str(timestamp)+extension)
     
 def main():
-
     dir_filename = 'huga/hoge.jpg'
     timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
     processing_filename(dir_filename, timestamp)
 
 if __name__ == '__main__':
-
     main()
